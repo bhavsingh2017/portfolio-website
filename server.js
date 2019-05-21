402f3e90b7d06c47853c80023cb026c3342806b7
bhavsingh2017 <bhavkirs@usc.edu>
2019-05-20 22:36:51 -0400

    about me page almost finished

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 
+
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
 	//
-  res.render('index', {title: "hello"});
+  res.render('home', {title: "Bhav's Portfolio"});
 });
 const server = app.listen(7000, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
 //this get function will be called when the guest method will be called
 
-
+app.use(express.static('public'));
 app.set('view engine', 'pug');
-document.getElementByID("myButton").onclick = ()=>{console.log("hey")};
+//document.getElementByID("myButton").onclick = ()=>{
+//    console.log("hey")
+//    };
 
+console.log(__dirname)
